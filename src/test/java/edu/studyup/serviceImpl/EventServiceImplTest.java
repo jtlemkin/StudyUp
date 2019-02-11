75e5739836ec1892ff7dfd449f851e6ef64bee18
jtlemkin <jtlemkin@gmail.com>
2019-02-10 22:28:12 -0800

    added unit tests

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -23,28 +23,6 @@ import edu.studyup.util.StudyUpException;
 class EventServiceImplTest {
 
 	EventServiceImpl eventServiceImpl;
-	
-	static Event createTestEvent(int eventID, String eventName) {
-		//Create Student
-		Student student = new Student();
-		student.setFirstName("John");
-		student.setLastName("Doe");
-		student.setEmail("JohnDoe@email.com");
-		student.setId(1);
-				
-		//Create Event1
-		Event event = new Event();
-		event.setEventID(eventID);
-		event.setDate(new Date());
-		event.setName(eventName);
-		Location location = new Location(-122, 37);
-		event.setLocation(location);
-		List<Student> eventStudents = new ArrayList<>();
-		eventStudents.add(student);
-		event.setStudents(eventStudents);
-		
-		return event;
-	}
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -57,7 +35,24 @@ class EventServiceImplTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		eventServiceImpl = new EventServiceImpl();
-		Event event = createTestEvent(1, "Event 1");
+		//Create Student
+		Student student = new Student();
+		student.setFirstName("John");
+		student.setLastName("Doe");
+		student.setEmail("JohnDoe@email.com");
+		student.setId(1);
+		
+		//Create Event1
+		Event event = new Event();
+		event.setEventID(1);
+		event.setDate(new Date());
+		event.setName("Event 1");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		List<Student> eventStudents = new ArrayList<>();
+		eventStudents.add(student);
+		event.setStudents(eventStudents);
+		
 		DataStorage.eventData.put(event.getEventID(), event);
 	}
 
@@ -79,5 +74,66 @@ class EventServiceImplTest {
 		Assertions.assertThrows(StudyUpException.class, () -> {
 			eventServiceImpl.updateEventName(eventID, "Renamed Event 3");
 		  });
+	}
+	
+	@Test
+	void getActiveEvents_allEventsAfterCurrentDate() {
+		Event pastEvent = new Event();
+		pastEvent.setEventID(2);
+		pastEvent.setDate(new Date(0));
+		
+		DataStorage.eventData.put(pastEvent.getEventID(), pastEvent);
+		
+		for(Event e: eventServiceImpl.getActiveEvents()) {
+			assertTrue(e.getDate().after(new Date()));
+		}
+	}
+	
+	@Test
+	void getActiveEvents_ensureNewEventInActiveEvents() {
+		Event futureEvent = new Event();
+		futureEvent.setEventID(2);
+		futureEvent.setDate(new Date((long) (System.currentTimeMillis() * 1.5)));
+		
+		DataStorage.eventData.put(futureEvent.getEventID(), futureEvent);
+		
+		assertTrue(eventServiceImpl.getActiveEvents().contains(futureEvent));
+	}
+	
+	@Test
+	void getPastEvents_allEventsBeforeCurrentDate() {
+		Event futureEvent = new Event();
+		futureEvent.setEventID(2);
+		futureEvent.setDate(new Date((long) (System.currentTimeMillis() * 1.5)));
+		
+		DataStorage.eventData.put(futureEvent.getEventID(), futureEvent);
+		
+		for(Event e: eventServiceImpl.getPastEvents()) {
+			assertTrue(e.getDate().before(new Date()));
+		}
+	}
+	
+	@Test
+	void getPastEvents_ensurePastEventInGetPastEvents() {
+		Event pastEvent = new Event();
+		pastEvent.setEventID(2);
+		pastEvent.setDate(new Date(0));
+		
+		DataStorage.eventData.put(pastEvent.getEventID(), pastEvent);
+		
+		assertTrue(eventServiceImpl.getPastEvents().contains(pastEvent));
+	}
+	
+	@Test
+	void deleteEvent_makeSureEventNotInDataStorage() {
+		eventServiceImpl.deleteEvent(1);
+		
+		assertFalse(DataStorage.eventData.containsKey(1));
+	}
+	
+	@Test
+	void deleteEvent_returnsDeletedEvent() {
+		Event event1 = DataStorage.eventData.get(1);
+		assertTrue(eventServiceImpl.deleteEvent(1) == event1);
 	}
 }
