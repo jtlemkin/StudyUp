4f1ec606c89312e67d0ce318894d449a7537a8f6
jtlemkin <jtlemkin@gmail.com>
2019-03-15 02:56:17 +0000

    fixed sed command and check for redis

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-if grep -Fq "$1" /etc/nginx/nginx.conf
+if grep -Fq "$1:" /etc/nginx/nginx.conf
 then
         echo "server is already located at $1"
 fi
 
-sed -i'' "s/(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/$1/" /etc/nginx/nginx.conf
+sed -i "s/redis:/$1:/" /etc/nginx/nginx.conf
+sed -i -r "s/(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/$1/" /etc/nginx/nginx.conf
 
 /usr/sbin/nginx -s reload
